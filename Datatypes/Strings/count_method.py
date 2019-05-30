dcb2cadef06b9d008f21c8c026620c3d344dda88
brahmaniraj <brahmaniraj@gmail.com>
2019-05-29 23:03:24 -0400

    String Methods

---
+++ Datatypes/Strings/count_method.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+str = "Welcome to python world"
+
+sub = "o"
+
+print("Varify the substring", str.count(sub),len(str))
+
+print("Varify the substring from a specific index ", str.count(sub, 10, 23))
+
+sub1 = "python"
+print ("str.count(sub1) : ", str.count(sub1))
+
--- Datatypes/Strings/format_method_stringspecial.py
+++ Datatypes/Strings/format_method_stringspecial.py
@@ -4,3 +4,6 @@ last_name = "moturu"
 
 print(" Firstname :{} Middle Name : {} Lastname : {}".format(first_name,middle_name,last_name))
 
+print("First Name : {0} Last Name :{1}  AccountID : {2}".format("Brahmani","Raj",10203040))
+
+print ("Value1 {a} Value2 {b} and Value3 {c}".format(a="python",b=100,c="pycharm"))
