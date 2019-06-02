cff124469c4029ee61199dfc56398ccf57a27d33
brahmaniraj <brahmaniraj@gmail.com>
2019-06-02 15:04:57 -0400

    strings methods

---
+++ Datatypes/Strings/isalnum.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+
+str1 = " "
+print("Alpha and numeric values test : ",str1.isalnum())
+
+str2="Python word ....!!!"
+print(str2.isalnum())
+
+a='20'
+b="ab"
+print(a.isalnum())
+print(b.isalnum())
+
+print(a.isdigit())
