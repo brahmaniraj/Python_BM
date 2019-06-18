b4bf24e6c304f0dafc48f95b2209263fd974885c
brahmaniraj <brahmaniraj@gmail.com>
2019-06-17 22:32:36 -0400

    exceptions

---
+++ Functions/Local_Global_Variables.py
@@ -0,0 +1,13 @@
+total = 0 # This is global variable.
+
+# Function definition is here
+def sum( arg1, arg2 ):
+   # Add both the parameters and return them."
+   total = arg1 + arg2 # Here total is local variable.
+   print("Inside the function local total : ", total)
+   return
+
+# Now you can call sum function
+sum( 10, 20 )
+
+print("Outside the function global total : ", total,id(total),type(total))
